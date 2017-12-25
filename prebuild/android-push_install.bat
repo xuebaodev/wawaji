db58c087b6f1e80c9b8ffd46e82a6ea425c628c3
xuebaodev <snowstrom@163.com>
2017-12-25 15:11:19 +0800

    Update README.md and APP

--- prebuild/android-push_install.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-adb install -r android-push.apk
-adb shell am start -n com.daniulive.smartpublisher/com.daniulive.smartpublisher.CameraPublishActivity
-ping -n 15 127.0.0.1>nul
-adb reboot
-exit
