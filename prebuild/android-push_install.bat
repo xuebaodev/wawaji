17ef55e9a6884ccc60f48bc4398c7affb42e12cb
xuebaodev <snowstrom@163.com>
2017-12-25 10:35:05 +0800

    fix a bug that file name wrong

--- prebuild/android-push_install.bat
+++ prebuild/android-push_install.bat
@@ -1,5 +1,5 @@
 @echo off
-adb install -r app-release.apk
+adb install -r android-push.apk
 adb shell am start -n com.daniulive.smartpublisher/com.daniulive.smartpublisher.CameraPublishActivity
 ping -n 15 127.0.0.1>nul
 adb reboot
