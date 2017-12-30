4fbbf85af8e3f54c9dd8cea16a3752a3dc9dc661
xuebaodev <snowstrom@163.com>
2017-12-30 11:29:53 +0800

    change filename

--- prebuild/android-push/1.android-push_install.bat
+++ prebuild/android-push/1.android-push_install.bat
@@ -1,5 +1,5 @@
 @echo off
-adb install -r android-push.apk
+adb install -r xuebaoPush.apk
 adb shell am start -n com.daniulive.smartpublisher/com.daniulive.smartpublisher.CameraPublishActivity
 ping -n 15 127.0.0.1>nul
 adb reboot
