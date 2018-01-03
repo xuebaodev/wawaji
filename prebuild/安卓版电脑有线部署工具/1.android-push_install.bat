994b33a427a94a8a34d88acb1952b51fa160678d
xuebaodev <snowstrom@163.com>
2018-01-03 13:41:20 +0800

    修改安装脚本的文件名

--- prebuild/安卓版电脑有线部署工具/1.android-push_install.bat
+++ prebuild/安卓版电脑有线部署工具/1.android-push_install.bat
@@ -1,5 +1,5 @@
 @echo off
-adb install -r xuebaoPush.apk
+adb install -r RTMPPush.apk
 adb shell am start -n com.daniulive.smartpublisher/com.daniulive.smartpublisher.CameraPublishActivity
 ping -n 15 127.0.0.1>nul
 adb reboot
